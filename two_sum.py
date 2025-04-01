27b47545bafc27898096bb80572899ac39b0804e
amirhossein bigdelu <amirbig44@gmail.com>
2025-04-01 16:47:21 +0330

    Add files via upload

---
+++ two_sum.py
@@ -0,0 +1,15 @@
+"""
+Two sum
+    [2, 11, 7, 15], 9 => [0, 2]
+    Complexity: O(n) - omega(1)
+"""
+def two_sum(numbers, target):
+    dic = {}
+    for i, num in enumerate(numbers):
+        if target - num in dic:
+            return [dic[target - num], i]
+        dic[num] = i
+    return None
+
+
+print(two_sum([2, 11, 7, 15], 9))
